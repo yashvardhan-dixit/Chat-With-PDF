6163dabc00d45bbd999cd6d122d8e12f56c5631a
yashvardhan-dixit <yvdakd55@gmail.com>
2025-11-26 21:11:06 +0530

    Fix import statement for RecursiveCharacterTextSplitter

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from pypdf import PdfReader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.embeddings import OpenAIEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain_openai import ChatOpenAI
